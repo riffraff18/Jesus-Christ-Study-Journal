122c8c67a39694c76c2ab4ac33167d6c778cab91
Rafferty Sorensen <raffertysorensen@hotmail.com>
2026-06-11 22:42:41 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('tg-study-full-v19').then(c=>c.addAll(['./','index.html','manifest.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('tg-study-jtc-v20').then(c=>c.addAll(['./','index.html','manifest.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
